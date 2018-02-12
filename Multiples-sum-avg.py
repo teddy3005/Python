29238f6d0b6ca5d5d628ce81d0409eced432f0f6
teddy3005 <tedkebede@yahoo.com>
2018-02-11 20:20:57 -0800

    updated the file

--- Multiples-sum-avg.py
+++ Multiples-sum-avg.py
@@ -81,21 +81,3 @@ b = multiply(a)
 print b
 
 
-from random import randint
-
-print "Scores and Grades"
-for count in range(0, 10):
-	score = randint(60, 100)
-
-	if(score <= 70):
-		grade = "D"
-	elif(score <= 80):
-		grade = "C"
-	elif(score <= 90):
-		grade = "B"
-	else:
-		grade = "A"
-
-	print "Score: %d; Your grade is %s" %(score, grade)
-
-print "End of program"
